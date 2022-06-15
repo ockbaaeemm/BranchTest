ae4fceb402867a745ae9d6e312762af8d8884e08
ockbaaeemm <bulyado@naver.com>
2022-06-15 14:45:40 +0900

    Add : add class
    CrashTest1 클래스 생성 및 코드 추가

---
+++ BranchTest/src/ct/CrashTest1.java
@@ -0,0 +1,17 @@
+package ct;
+
+public class CrashTest1 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		String b = "1";
+		int c = 2;
+		String d = "2";
+		
+		System.out.println("int a + String b = " + a + b);
+		System.out.println("int c + String d = " + c + d);
+
+	}
+
+}
