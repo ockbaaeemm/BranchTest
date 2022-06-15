e0015516e3a0784f3a8e12de253f3a02c4621742
ockbaaeemm <bulyado@naver.com>
2022-06-15 17:12:59 +0900

    Add : CrashTest1 Class
    CrashTest 클래서 코드 추가

--- BranchTest/src/ct/CrashTest1.java
+++ BranchTest/src/ct/CrashTest1.java
@@ -4,16 +4,33 @@ public class CrashTest1 {
 
 	public static void main(String[] args) {
 		
-		int x = 1;
+		int a = 1;
 		String y = "1";
-		int c = 2;
+		int b = 2;
 		String d = "2";
 		
-		System.out.println("int x + String y = " + x + y);
-		c = 2;
-		d = "2";
+		System.out.println("int x + String y = " + a + y);
 		
-		System.out.println("int c + String d = " + c + d);
+		System.out.println("int c + String d = " + b + d);
+		
+		int sum1 = 0;
+		int sum2 = 0;
+		
+		for(int i=0; i<73; i++) {
+			
+			sum1 += i;
+			
+		}
+		
+		for(int x=0; x<100; x++) {
+					
+					sum2 += x;
+					
+				}
+		
+		System.out.println("합계 1 : " + sum1);
+		
+		System.out.println("합계 1 : " + sum2);
 
 	}
 
