a66d1dd1f50c1f33154b6a4a1377b540b048245b
ockbaaeemm <bulyado@naver.com>
2022-06-15 14:47:46 +0900

    Rename : CrashTest1
    int a, String b 변수 이름 변경

--- BranchTest/src/ct/CrashTest1.java
+++ BranchTest/src/ct/CrashTest1.java
@@ -4,12 +4,12 @@ public class CrashTest1 {
 
 	public static void main(String[] args) {
 		
-		int a = 1;
-		String b = "1";
+		int x = 1;
+		String y = "1";
 		int c = 2;
 		String d = "2";
 		
-		System.out.println("int a + String b = " + a + b);
+		System.out.println("int x + String y = " + x + y);
 		System.out.println("int c + String d = " + c + d);
 
 	}
